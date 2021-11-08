6de88313a175457a231f27df1af3cb190379018c
Dresvyanskiy <denis.dresvyanskiy@uni-ulm.de>
2021-11-08 14:34:54 +0100

    The script for the video acquisition now works fine.

--- src/video_acquisition.py
+++ src/video_acquisition.py
@@ -1,7 +1,8 @@
+import os
 from argparse import ArgumentParser
 from typing import Tuple
 
-from lib.pyk4a import Config, PyK4A, PyK4ARecord, DepthMode, ColorResolution, FPS, ImageFormat, WiredSyncMode
+from pyk4a import Config, PyK4A, PyK4ARecord, DepthMode, ColorResolution, FPS, ImageFormat, WiredSyncMode
 
 
 def device_initialization(color_resolution: ColorResolution = ColorResolution.RES_720P,
@@ -39,21 +40,49 @@ def run_recording(device:PyK4A, config:Config, path_to_output_filename:str):
 
 
 if __name__=="__main__":
+    import argparse
+    # parsing
+    parser=argparse.ArgumentParser(description='Video and depth data acquisitions using the Azure Kinect.')
+    parser.add_argument('--res', action='store', type=int, default=ColorResolution.RES_720P)
+    parser.add_argument('--color_format', action='store', type=int, default=ImageFormat.COLOR_MJPG)
+    parser.add_argument('--depth_mode', action='store', type=int, default=DepthMode.NFOV_2X2BINNED)
+    parser.add_argument('--fps', action='store', type=int, default=FPS.FPS_30)
+    parser.add_argument('--synchro', action='store', type=bool, default=True)
+    parser.add_argument('--device_id', action='store', type=int, default=0)
+    parser.add_argument('--output_path', action='store', type=str, default='results')
+    parser.add_argument('--rec_time', action='store', type=int, default=1800) #(30 minutes)
+    args=parser.parse_args()
+    # device and device config initialization
+    device, config=device_initialization(color_resolution=args.res,
+                                         color_format=args.color_format,
+                                         camera_fps=args.fps,
+                                         synchronized_images_only=args.synchro,
+                                         device_id=args.device_id)
 
-    filename = "video_test.mkv"
+    # check that all the arguments are passed rightly
+    if args.fps==0:
+        fps=5
+    elif args.fps==1:
+        fps=15
+    elif args.fps==2:
+        fps=30
+    else:
+        raise Exception("Invalid argument for fps parameter.")
 
-    device, config=device_initialization()
+    # create the output directory if needed
+    if not os.path.exists(args.output_path):
+        os.makedirs(args.output_path, exist_ok=True)
 
-    record=run_recording(device, config, path_to_output_filename=filename)
-
-    i = 0
+    # start recording
+    record = run_recording(device, config, path_to_output_filename=os.path.join(args.output_path, "result.mkv"))
+    recorded_frames = 0
     try:
         print("Recording... Press CTRL-C to stop recording.")
         while True:
             capture = device.get_capture()
             record.write_capture(capture)
-            i += 1
-            if i == 1800:
+            recorded_frames += 1
+            if recorded_frames == args.rec_time*fps:
                 break
     except KeyboardInterrupt:
         print("CTRL-C pressed. Exiting.")
