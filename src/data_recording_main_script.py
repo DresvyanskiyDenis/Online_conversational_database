0b6b59b0249b4287e6dca225a130c46aa9c8d5d0
Dresvyanskiy <denis.dresvyanskiy@uni-ulm.de>
2021-11-10 16:39:37 +0100

    THe procedure of anonymization is added. Now the main script is able to run from cmd with specifying argument, including the output path.

--- src/data_recording_main_script.py
+++ src/data_recording_main_script.py
@@ -1,8 +1,24 @@
 import subprocess
 import os
 import src.data_recording_config as rec_config
+import shutil
+
+from src.anonimization import generate_pseudo_anonimization
 
 if __name__ == '__main__':
+    import argparse
+    # parsing
+    parser = argparse.ArgumentParser(description='Data recording with Azure Kinect and external Microphone.')
+    parser.add_argument('--name', action='store', type=str, required=True)
+    parser.add_argument('--surname', action='store', type=str, required=True)
+    parser.add_argument('--birth_date', action='store', type=str, required=True)
+    parser.add_argument('--session_num', action='store', type=str, required=True)
+    parser.add_argument('--date', action='store', type=str, required=True)
+    parser.add_argument('--output_path', action='store', type=str, required=True)
+    args = parser.parse_args()
+
+
+
     cmd_for_kinect = "python video_acquisition.py --res %i --color_format %i --depth_mode %i --fps %i " \
                      "--synchro %r --device_id %i --output_path %s --rec_time %i"% \
                      (rec_config.KINECT_RESOLUTION, rec_config.KINECT_IMAGE_FORMAT, rec_config.KINECT_DEPTH_MODE,
@@ -35,3 +51,39 @@ if __name__ == '__main__':
     processes.append(p)
     for process in processes:
         process.wait()
+
+
+    # reallocate the recorded files to the output path
+    # create folder if does not exists
+    if not os.path.exists(args.output_path):
+        os.makedirs(args.output_path, exist_ok=True)
+    # create folder for session if does not exasts
+    day, month, year = args.date.split('.')
+    session_full_name= 'Session_%i_date_%s_%s_%s'%(int(args.session_num), day, month, year)
+    if not os.path.exists(os.path.join(args.output_path,session_full_name)):
+        os.makedirs(os.path.join(args.output_path,session_full_name), exist_ok=True)
+
+    # generate pseudoanonimized name
+    pseudo_name=generate_pseudo_anonimization(name=args.name, surname=args.surname, birth_date=args.birth_date)
+
+
+    # reallocate files
+    path_for_moving = os.path.join(args.output_path,session_full_name, pseudo_name)
+    # create the folder if does not exists
+    if not os.path.exists(path_for_moving):
+        os.makedirs(path_for_moving)
+    # Moving files from Kinect
+    filenames=os.listdir(rec_config.KINECT_OUTPUT_PATH)
+    for filename in filenames:
+        shutil.move(os.path.join(rec_config.KINECT_OUTPUT_PATH, filename), os.path.join(path_for_moving, filename))
+
+    # Moving files from Audio-Kinect
+    filenames = os.listdir(rec_config.AUDIO_KINECT_OUTPUT_PATH)
+    for filename in filenames:
+        shutil.move(os.path.join(rec_config.AUDIO_KINECT_OUTPUT_PATH, filename), os.path.join(path_for_moving, filename))
+
+    # Moving files from External audio device
+    filenames = os.listdir(rec_config.AUDIO_MICROPHONE_OUTPUT_PATH)
+    for filename in filenames:
+        shutil.move(os.path.join(rec_config.AUDIO_MICROPHONE_OUTPUT_PATH, filename), os.path.join(path_for_moving, filename))
+
