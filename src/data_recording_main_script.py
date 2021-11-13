7ccb3236a80536b1ecc2a348b1fc1d8a15c9ee72
Dresvyanskiy <denis.dresvyanskiy@uni-ulm.de>
2021-11-13 16:23:59 +0100

    Automatic addition of the module src to the sys.path was implemented.

--- src/data_recording_main_script.py
+++ src/data_recording_main_script.py
@@ -1,5 +1,10 @@
-import subprocess
 import os
+import sys
+module_path = os.path.abspath(os.getcwd())
+if module_path not in sys.path:
+    sys.path.append(module_path)
+
+import subprocess
 import src.data_recording_config as rec_config
 import shutil
 
@@ -19,14 +24,14 @@ if __name__ == '__main__':
 
 
 
-    cmd_for_kinect = "python video_acquisition.py --res %i --color_format %i --depth_mode %i --fps %i " \
+    cmd_for_kinect = "python src\\video_acquisition.py --res %i --color_format %i --depth_mode %i --fps %i " \
                      "--synchro %r --device_id %i --output_path %s --rec_time %i"% \
                      (rec_config.KINECT_RESOLUTION, rec_config.KINECT_IMAGE_FORMAT, rec_config.KINECT_DEPTH_MODE,
                       rec_config.KINECT_FPS, rec_config.KINECT_SYNCHRO, rec_config.KINECT_DEVICE_ID,
                       rec_config.KINECT_OUTPUT_PATH, rec_config.KINECT_RECORDING_TIME)
 
 
-    cmd_for_kinect_audio = "python audio_recording.py --format %s --rate %i --num_channels %i " \
+    cmd_for_kinect_audio = "python src\\audio_recording.py --format %s --rate %i --num_channels %i " \
                            "--device_index %i --chunk_size %i --time %i " \
                            "--output_path %s --output_filename %s"%(rec_config.AUDIO_KINECT_FORMAT, rec_config.AUDIO_KINECT_RATE,
                                                                     rec_config.AUDIO_KINECT_NUM_CHANNELS, rec_config.AUDIO_KINECT_DEVICE_INDEX,
@@ -34,7 +39,7 @@ if __name__ == '__main__':
                                                                     rec_config.AUDIO_KINECT_OUTPUT_PATH, rec_config.AUDIO_KINECT_OUTPUT_FILENAME)
 
     # add cmd for the external audio recorded (microphone)
-    cmd_for_external_audio = "python audio_recording.py --format %s --rate %i --num_channels %i " \
+    cmd_for_external_audio = "python src\\audio_recording.py --format %s --rate %i --num_channels %i " \
                            "--device_index %i --chunk_size %i --time %i " \
                            "--output_path %s --output_filename %s"%(rec_config.AUDIO_MICROPHONE_FORMAT, rec_config.AUDIO_MICROPHONE_RATE,
                                                                     rec_config.AUDIO_MICROPHONE_NUM_CHANNELS, rec_config.AUDIO_MICROPHONE_DEVICE_INDEX,
