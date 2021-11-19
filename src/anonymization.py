b2813624e1fd6b6176c33ed3495829592ff450a3
Dresvyanskiy <denis.dresvyanskiy@uni-ulm.de>
2021-11-19 16:50:42 +0100

    THe birth date was changed to just today_date

--- src/anonymization.py
+++ src/anonymization.py
@@ -1,16 +1,16 @@
 import re
 
-def generate_pseudo_anonymization(name:str, surname:str, birth_date:str):
+def generate_pseudo_anonymization(name:str, surname:str, date_of_recording:str):
 
-    birth_date=birth_date.strip()
+    date_of_recording=date_of_recording.strip()
     name=name.strip()
     surname=surname.strip()
 
     pattern = re.compile("\d\d\.\d\d\.\d\d\d\d")
-    if not pattern.match(birth_date):
-        raise Exception('THe format for passig the date is dd.mm.yyyy. You have passed: %s'%(birth_date))
+    if not pattern.match(date_of_recording):
+        raise Exception('THe format for passing the date is dd.mm.yyyy. You have passed: %s'%(date_of_recording))
 
-    day, month, year = birth_date.split('.')
+    day, month, year = date_of_recording.split('.')
     return name[0].lower()+name[-1].lower() \
            + str(int(day)*int(month)*int(year)) \
            + surname[0].lower()+surname[-1].lower()
