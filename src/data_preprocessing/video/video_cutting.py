1dee90052e63a1b9addb11395b8413da7420c716
Denis <ecodenis@yandex.ru>
2022-12-20 10:46:39 +0100

    A function for cutting the video on 1, 15, and 60 seconds segments as well as combining the 15 and 60 seconds segments into one video has been created. It should be tested.

--- src/data_preprocessing/video/video_cutting.py
+++ src/data_preprocessing/video/video_cutting.py
@@ -1,5 +1,5 @@
 import os
-from typing import Optional, Tuple
+from typing import Optional, Tuple, List
 
 import numpy as np
 import pandas as pd
@@ -50,6 +50,83 @@ def cut_one_video_on_sequences(path_to_video:str, sequence_length:float, output_
     metadata.to_csv(os.path.join(output_path, "metadata.csv"), index=False)
 
 
+def cut_videos_and_combine_them(paths:List[str], output_path:str, resize:Optional[Tuple[int, int]]=None)->None:
+    # TODO: CHECK THIS FUNCTION
+    """ Cuts videos on sequences of a given length and combines them into one video.
+
+    :param paths: List[str]
+        List of paths to the videos.
+    :param sequence_length: float
+        Length of the sequences in seconds.
+    :param output_path: str
+        Path to the output folder.
+    :param resize: Optional[Tuple[int, int]]
+        Tuple with the new width and height of the video, if specified.
+    :return: None
+    """
+
+    # create output dir if needed
+    if not os.path.exists(output_path):
+        os.makedirs(output_path, exist_ok=True)
+
+    # cut videos on sequences
+    sequences = [1., 15., 60.]
+    filenames = [path.split(os.path.sep)[-1].split('.')[0] for path in paths]
+    for path in paths:
+        for sequence_length in sequences:
+            filename = path.split(os.path.sep)[-1].split('.')[0]
+            final_output_path = os.path.join(output_path, "%s_sequence_length_%f"%(filename, sequence_length))
+            cut_one_video_on_sequences(path, sequence_length, output_path=final_output_path,
+                                   resize=resize)
+    # combine 15- and 60-seconds sequences into one video
+    combined_15_output_path = os.path.join(output_path, "combined_15_sec")
+    combined_60_output_path = os.path.join(output_path, "combined_60_sec")
+    # create output dirs if needed
+    if not os.path.exists(combined_15_output_path):
+        os.makedirs(combined_15_output_path, exist_ok=True)
+    if not os.path.exists(combined_60_output_path):
+        os.makedirs(combined_60_output_path, exist_ok=True)
+    # open metadata files
+    metadata_15_seconds_file_0 = pd.read_csv(os.path.join(output_path, "%s_sequence_length_%f"%(filenames[0], 15), "metadata.csv"))
+    metadata_15_seconds_file_1 = pd.read_csv(os.path.join(output_path, "%s_sequence_length_%f"%(filenames[1], 15), "metadata.csv"))
+    metadata_15_seconds_file_2 = pd.read_csv(os.path.join(output_path, "%s_sequence_length_%f"%(filenames[2], 15), "metadata.csv"))
+    metadata_60_seconds_file_0 = pd.read_csv(os.path.join(output_path, "%s_sequence_length_%f"%(filenames[0], 60), "metadata.csv"))
+    metadata_60_seconds_file_1 = pd.read_csv(os.path.join(output_path, "%s_sequence_length_%f"%(filenames[1], 60), "metadata.csv"))
+    metadata_60_seconds_file_2 = pd.read_csv(os.path.join(output_path, "%s_sequence_length_%f"%(filenames[2], 60), "metadata.csv"))
+    # combine 15-seconds sequences
+    combined_files_metadata = pd.DataFrame(columns=['video_name', 'sequence_ID', 'start_time', 'end_time'])
+    for idx in range(len(metadata_15_seconds_file_0)):
+        compose_three_videos(video1=os.path.join(output_path, "%s_sequence_length_%f"%(filenames[0], 15), "%s_seq_%i.mp4"%(filenames[0], idx)),
+                             video2=os.path.join(output_path, "%s_sequence_length_%f"%(filenames[1], 15), "%s_seq_%i.mp4"%(filenames[1], idx)),
+                             video3=os.path.join(output_path, "%s_sequence_length_%f"%(filenames[2], 15), "%s_seq_%i.mp4"%(filenames[2], idx)),
+                             final_resolution=(1920, 1080),
+                             save_video=True,
+                             output_path = os.path.join(combined_15_output_path, "combined_seq_%i.mp4"%idx))
+        combined_files_metadata = combined_files_metadata.append({'video_name': "combined_seq_%i.mp4"%idx,
+                                                                    'sequence_ID': idx,
+                                                                    'start_time': metadata_15_seconds_file_0.iloc[idx]['start_time'],
+                                                                    'end_time': metadata_15_seconds_file_0.iloc[idx]['end_time']
+                                                                    }, ignore_index=True)
+    # write metadata to the output path
+    combined_files_metadata.to_csv(os.path.join(combined_15_output_path, "metadata.csv"), index=False)
+    # combine 60-seconds sequences
+    combined_files_metadata = pd.DataFrame(columns=['video_name', 'sequence_ID', 'start_time', 'end_time'])
+    for idx in range(len(metadata_60_seconds_file_0)):
+        compose_three_videos(video1=os.path.join(output_path, "%s_sequence_length_%f"%(filenames[0], 60), "%s_seq_%i.mp4"%(filenames[0], idx)),
+                             video2=os.path.join(output_path, "%s_sequence_length_%f"%(filenames[1], 60), "%s_seq_%i.mp4"%(filenames[1], idx)),
+                             video3=os.path.join(output_path, "%s_sequence_length_%f"%(filenames[2], 60), "%s_seq_%i.mp4"%(filenames[2], idx)),
+                             final_resolution=(1920, 1080),
+                             save_video=True,
+                             output_path = os.path.join(combined_60_output_path, "combined_seq_%i.mp4"%idx))
+        combined_files_metadata = combined_files_metadata.append({'video_name': "combined_seq_%i.mp4"%idx,
+                                                                    'sequence_ID': idx,
+                                                                    'start_time': metadata_60_seconds_file_0.iloc[idx]['start_time'],
+                                                                    'end_time': metadata_60_seconds_file_0.iloc[idx]['end_time']
+                                                                    }, ignore_index=True)
+    # write metadata to the output path
+    combined_files_metadata.to_csv(os.path.join(combined_60_output_path, "metadata.csv"), index=False)
+
+
 
 
 
@@ -75,4 +152,6 @@ if __name__ == '__main__':
 
     compose_three_videos(video1=video_1, video2=video_2, video3=video_3, final_resolution=(1920, 1080),
     save_video = True, output_path = output_path)
+
+
 
